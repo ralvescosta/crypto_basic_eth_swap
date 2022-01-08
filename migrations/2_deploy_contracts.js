f59ae609d03282859d243967f91a747c7ccca61e
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-08 09:09:27 -0300

    refactor: deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,15 @@ const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  // deploy token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  // deploy ethSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // transfer tokens to ethSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
